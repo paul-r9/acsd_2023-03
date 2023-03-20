985a9a6e95a82b7cd57c5e5a7fa708e948af37f2
Paul Moore <pdmoore@gmail.com>
2023-03-20 16:43:55 -0700

    100% coverage with 3 tests in c#

---
+++ breakout_1/ATP_coveredgr/csharp/GildedRose/OneHundredPercentCoverage.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GildedRose
+{  
+    // An example of how 100% Code Coverage doesn't correlate with 100% behavior coverage
+    public class OneHundredPercentCoverage
+    {
+        private List<Item> createItemList(string itemName, int sellIn, int quality) {
+            return new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
+        }        
+
+        // 42%
+        [Test]
+        public void AgedBrie_PastSellInDate_QualityBelowMax_QualityIncreasesTwiceAsFast() {
+            GildedRose sut = new GildedRose(createItemList("Aged Brie", -1, 40));
+    
+            sut.UpdateQuality();
+    
+            Assert.AreEqual(42, sut.Items[0].Quality, "Quality increases twice as fast for old cheese");
+        }
+
+
+        // 67%
+        [Test]
+        public void BackstagePass_PastSellInDate_QualityDropsToZero()
+        {
+            GildedRose sut = new GildedRose(createItemList("Backstage passes to a TAFKAL80ETC concert", -1, 40));
+    
+            sut.UpdateQuality();
+    
+            Assert.AreEqual(0, sut.Items[0].Quality, "Backstage pass after the concert has no value");
+        }
+
+        // 100%
+        [Test]
+        public void AnyItem_PastSellInDate_QualityDropsTwiceAsFast()
+        {
+            GildedRose sut = new GildedRose(createItemList("any item", -1, 30));
+    
+            sut.UpdateQuality();
+    
+            Assert.AreEqual(28, sut.Items[0].Quality, "quality drops twice as fast after sellin date");
+        }
+        
+    }
+}
